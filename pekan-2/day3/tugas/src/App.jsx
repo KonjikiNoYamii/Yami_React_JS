a1801333d374ab1d8388db7bbf9eedebe7b0f6da
KonjikiNoYami <zidanalbani97gmail.com>
2025-10-10 16:54:05 +0700

    done

--- pekan-2/day3/tugas/src/App.jsx
+++ pekan-2/day3/tugas/src/App.jsx
@@ -5,11 +5,20 @@ import DoubleEffect from "./components/DoubleEffect"
 import FetchSimulation from "./components/FetchSimulation"
 export default function App() {
   return(
-    <div>
+    <div style={{justifySelf:"center"}}>
+      <h1>soal 1</h1>
       <SimpleEffect />
+      <hr />
+      <h1>soal 2</h1>
       <ChangeName />
+      <hr />
+      <h1>soal 3</h1>
       <ChangeWindow />
+      <hr />
+      <h1>soal 4</h1>
       <DoubleEffect />
+      <hr />
+      <h1>soal 5</h1>
       <FetchSimulation />
     </div>
   )
--- pekan-2/day3/tugas/src/components/FetchSimulation.jsx
+++ pekan-2/day3/tugas/src/components/FetchSimulation.jsx
@@ -1,38 +1,46 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function FetchSimulation() {
-    const [userId, setUserId] =useState(1)
-    const [user, setUser] =useState(null)
-    const [loading, setLoading] = useState(false)
+  const [userId, setUserId] = useState(1);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-
-    useEffect(() =>{
-        setLoading(true)
-            fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
         .then((res) => res.json())
-        .then((data) =>{
-            setUser(data)
-            setLoading(false)
+        .then((data) => {
+          setUser(data);
+          setLoading(false);
         })
-        .catch((err) =>{
-            console.error(err);
-            
-        })
-    },[userId])
+        .catch((err) => {
+          console.error(err);
+          setLoading(false);
+        });
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [userId]);
 
-    if (loading) return <p>sedang memuat.....</p>
-    if (!user) return <p>pengguna tidak ditemukan</p>
+  if (loading) return <p>sedang memuat...</p>;
+  if (!user) return <p>pengguna tidak ditemukan!</p>;
 
-    return(
-        <div>
-            <h2>data pengguna</h2>
-            <p>{userId}</p>
-            <p>{user.name}</p>
-            <p>{user.email}</p>
+  return (
+    <div>
+      <h2>Data Pengguna</h2>
+      <p>
+        <b>ID:</b> {user.id}
+      </p>
+      <p>
+        <b>Nama:</b> {user.name}
+      </p>
+      <p>
+        <b>Email:</b> {user.email}
+      </p>
 
-            <button onClick={() => setUserId(prev => prev +1)}>see next user</button>
-        </div>
-    )
+      <button onClick={() => setUserId((prev) => prev + 1)}>
+        Lihat Pengguna Selanjutnya
+      </button>
+    </div>
+  );
 }
