cff7797bd975afd38f5a143c9cd8dab1ed843da2
KonjikiNoYami <zidanalbani97gmail.com>
2025-10-20 16:42:21 +0700

    sudah

--- pekan-3/day5/tugas/src/pages/NotFound.jsx
+++ pekan-3/day5/tugas/src/pages/NotFound.jsx
@@ -2,8 +2,6 @@ export default function NotFound() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <a href="/">Go Home</a>
     </div>
   );
 }
